7c9f6c041543b2269ffb7e8a6dc280a5335a2e5a
Priyarikson <priyankaraju92@gmail.com>
2022-11-24 23:56:04 -0500

    stage1

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ function addItem() {
     newItem.onclick = removeItem;
     // document.location.appendChild(newItem);
     document.getElementById("list").appendChild(newItem);
+    saveList();
 }
 function removeItem() {
     // (this) keyword
     document.getElementById("list").removeChild(this);
+    saveList();
 }
+function saveList() {
+    // localStorage.storageName = "";  //remove piece of information from local storage
+    // localStorage keyword.localStorage name(giving a name to the information you want to store) = "information stored(the information u want to store)"
+    //localStorage.storedList //keyword.name
+    localStorage.storedList = document.getElementById("list").innerHTML;
+}
+function loadList() {
+    document.getElementById("list").innerHTML = localStorage.storedList;
+    for(let i = 0; i < list.children.length; i++) {
+        list.children[i].onclick = removeItem;
+    }
+}
+//  Check if there is already a vlaue in local storage //little confused about this explanation
+//if(localStorage.storedList) {
+    //loadList();
+//}
